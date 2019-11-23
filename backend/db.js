677d07d54d514ad93f90a1ee45ada0e0449d323c
oshnix <sdsdaniil@gmail.com>
2019-11-23 13:52:24 +0300

    fixed db connection

--- backend/db.js
+++ backend/db.js
@@ -6,9 +6,13 @@ mongoose.connect(
     'mongodb://127.0.0.1:27017/polina',
     { useNewUrlParser: true, user: "polina", pass: "1234" }
 ).then(() => {
-    FavoriteCity = mongoose.model('FavoriteCity', new mongoose.Schema({
-        _id: String //it is the name
-    }));
+    FavoriteCity = mongoose.model(
+        'FavoriteCity',
+        new mongoose.Schema(
+            { _id: String },
+            { collection: 'favorites' }
+        )
+    );
 });
 
 async function insert(name) {
@@ -22,12 +26,11 @@ async function deleteCity(name) {
     await FavoriteCity.deleteOne({ _id: name });
 }
 
-function selectAll() {
+async function selectAll() {
     return new Promise((resolve, reject) => {
         FavoriteCity.find((err, favorites) => {
-		console.log(favorites);
             if (err) {
-		    console.error(err);
+		        console.error(err);
                 reject(err);
             }
             resolve(favorites);
